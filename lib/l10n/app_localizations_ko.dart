ab0aa616f09332da7b2cf85d34cb4cf4c2cd8db0
Levi <levi@blueverse.kr>
2024-08-02 12:16:47 +0700

    Update app_localizations_ko.dart

--- lib/l10n/app_localizations_ko.dart
+++ lib/l10n/app_localizations_ko.dart
@@ -5,5 +5,5 @@ class AppLocalizationsKo extends AppLocalizations {
   AppLocalizationsKo([String locale = 'ko']) : super(locale);
 
   @override
-  String get app_name => 'AAA';
+  String get app_name => '앱';
 }
