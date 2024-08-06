40e6f862c8a5b73bcee2f1825e911f9fbb8b7a0d
Nguyễn Bảo Trung <baotrung372k@gmail.com>
2024-08-06 14:55:44 +0700

    add use case base

--- lib/domain/use_cases/base/use_case.dart
+++ lib/domain/use_cases/base/use_case.dart
@@ -5,3 +5,11 @@ abstract class FutureUseCase<Input, Output> {
 abstract class FutureOutputUseCase<Output> {
   Future<Output> run();
 }
+
+abstract class UseCase<Input, Output> {
+  Output run(Input input);
+}
+
+abstract class OutputUseCase<Output> {
+  Output run();
+}
