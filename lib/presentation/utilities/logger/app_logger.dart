8af797b1269d6b2e87e387101e719177cead8d79
Levi <levi@blueverse.kr>
2024-08-07 16:03:31 +0700

    remove properties
    Co-authored-by: BaoTrung37 <baotrung372k@gmail.com>
    Co-authored-by: Levi <levi@blueverse.kr>

--- lib/presentation/utilities/logger/app_logger.dart
+++ lib/presentation/utilities/logger/app_logger.dart
@@ -2,8 +2,6 @@ import 'package:logger/logger.dart';
 
 final lg = Logger(
   printer: PrettyPrinter(
-    errorMethodCount: 5,
-    lineLength: 50,
     colors: true,
     printEmojis: true,
     dateTimeFormat: DateTimeFormat.dateAndTime,
