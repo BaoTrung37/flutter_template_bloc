a5069655c1ef076b4bd8e2a915d52c1f1b43fcf6
Levi <levi@blueverse.kr>
2024-08-15 15:27:25 +0700

    add access token
    Co-Authored-By: Nguyễn Bảo Trung <baotrung372k@gmail.com>

--- lib/data/services/network_service/interceptors/auth_interceptor.dart
+++ lib/data/services/network_service/interceptors/auth_interceptor.dart
@@ -1,11 +1,16 @@
 import 'package:dio/dio.dart';
+import 'package:example_flutter_app/data/data.dart';
 import 'package:injectable/injectable.dart';
 
 @injectable
 class AuthInterceptor extends Interceptor {
+  AuthInterceptor(this._sharedManager);
+
+  final SharedManager _sharedManager;
   @override
   void onRequest(RequestOptions options, RequestInterceptorHandler handler) {
-    // options.headers['Authorization'] = 'Bearer $token';
+    final token = _sharedManager.getAccessToken();
+    options.headers['Authorization'] = 'Bearer $token';
     handler.next(options);
   }
 }
