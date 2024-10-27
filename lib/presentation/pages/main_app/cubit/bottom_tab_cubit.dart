016cc32b7a8f93da604eb9f8c1fe0c4920f5af5c
Nguyễn Bảo Trung <baotrung372k@gmail.com>
2024-10-27 21:22:53 +0700

    change lazysingleton to injectable

--- lib/presentation/pages/main_app/cubit/bottom_tab_cubit.dart
+++ lib/presentation/pages/main_app/cubit/bottom_tab_cubit.dart
@@ -1,9 +1,11 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:injectable/injectable.dart';
 
-@lazySingleton
+@injectable
 class BottomTabCubit extends Cubit<int> {
   BottomTabCubit() : super(0);
 
-  void changeTab(int tabIndex) => emit(tabIndex);
+  void changeTab(int tabIndex) {
+    emit(tabIndex);
+  }
 }
