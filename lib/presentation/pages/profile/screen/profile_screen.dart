a5ebbd6b537bf5911de0fe8449a03d7ce7518668
Levi <levi@blueverse.kr>
2025-06-19 15:05:19 +0700

    add setting screen

--- lib/presentation/pages/profile/screen/profile_screen.dart
+++ lib/presentation/pages/profile/screen/profile_screen.dart
@@ -1,9 +1,6 @@
 import 'package:auto_route/auto_route.dart';
-import 'package:example_flutter_app/core/application/language/language_bloc.dart';
-import 'package:example_flutter_app/core/shared/enum/lanugages.dart';
-import 'package:example_flutter_app/injection/di.dart';
+import 'package:example_flutter_app/core/router/app_router.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 
 @RoutePage()
 class ProfileScreen extends StatelessWidget {
@@ -14,30 +11,19 @@ class ProfileScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Profile Screen'),
+        actions: [
+          IconButton(
+            onPressed: () {
+              context.router.push(const SettingRoute());
+            },
+            icon: const Icon(Icons.settings),
+          ),
+        ],
       ),
-      body: Center(
+      body: const Center(
         child: Column(
           children: [
-            BlocBuilder<LanguageBloc, LanguageState>(
-              bloc: getIt<LanguageBloc>(),
-              builder: (context, state) {
-                return DropdownButton<Languages>(
-                  items: Languages.values
-                      .map(
-                        (language) => DropdownMenuItem(
-                          value: language,
-                          child: Text(language.name),
-                        ),
-                      )
-                      .toList(),
-                  value: state.language,
-                  onChanged: (value) {
-                    getIt<LanguageBloc>()
-                        .add(LanguageEvent.changeTempLanguage(value!));
-                  },
-                );
-              },
-            ),
+            Text('Profile Screen'),
           ],
         ),
       ),
